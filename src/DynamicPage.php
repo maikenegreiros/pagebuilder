82aaca40c54e0df0684e1d4d6b469a9c08327f2b
Maike <maike_jbe@hotmail.com>
2018-01-25 17:11:35 -0300

    Criada classe concreta para montar páginas dinâmicas

---
+++ src/DynamicPage.php
@@ -0,0 +1,49 @@
+<?php
+namespace MaikeNegreiros;
+
+use MaikeNegreiros\PageBuilder;
+use Ciebit\HpEngenharia\Views\View;
+use Ciebit\HpEngenharia\Models\Model;
+
+class DynamicPage extends PageBuilder
+{
+    private $model_element_selector = '.model-element';
+    private $View; #View
+    private $Model; #Model
+
+    public function __construct(View $view, Model $model)
+    {
+        $this->View = $view;
+        $this->Model = $model;
+    }
+
+    public function buildHtml(): self
+    {
+        $document = $this->getTemplateDocument();
+        $container = $document->querySelector($this->selectorNodeWhereToImport);
+        $document2 = $this->View->getDocumentUpdated($this->Model);
+        $content = $document2->querySelector($this->selectorNodeToImport);
+        $fragment = $document->importNode($content, true);
+        $container->appendChild($fragment);
+
+        $this->importMetaTagsAndTitle('title');
+        $this->importMetaTagsAndTitle('[name=description]');
+        $this->importMetaTagsAndTitle('[name=keywords]');
+        $this->importStylesAndScripts();
+
+        $this->output = $document->saveHTML();
+
+        return $this;
+    }
+
+    public function getModelElementSelector(): string
+    {
+        return $this->model_element_selector;
+    }
+
+    public function setModelElementSelector(string $selector): self
+    {
+        $this->model_element_selector = $selector;
+        return $this;
+    }
+}
